6eff8afd95dd58a38015a0fc718bbca81e3f34ff
Sadegh Zeidanlou <46355431+sadeghzeidanlou@users.noreply.github.com>
2023-06-20 11:22:24 +0330

    Set some entity configurations

--- RiceMill.Persistence/Configurations/ConcernConfiguration.cs
+++ RiceMill.Persistence/Configurations/ConcernConfiguration.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RiceMill.Domain.Models;
 
 namespace RiceMill.Persistence.Configurations
 {
-    internal class ConcernConfiguration
+    internal class ConcernConfiguration : IEntityTypeConfiguration<Concern>
     {
+        public void Configure(EntityTypeBuilder<Concern> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Title)
+                .HasMaxLength(50)
+                .IsUnicode()
+                .IsRequired();
+
+            builder.Property(c => c.IsDeleted)
+                .HasDefaultValue(false);
+
+            builder.Property(c => c.CreateTime)
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.Property(c => c.UpdateTime)
+                .HasDefaultValueSql("GETDATE()");
+
+            builder
+                .HasMany(c => c.Payment)
+                .WithOne(c => c.Concern);
+
+            builder
+                .HasOne(c => c.User)
+                .WithMany(c => c.Concerns);
+
+            builder
+                .HasOne(c => c.RiceMill)
+                .WithMany(c => c.Concerns);
+        }
     }
 }
