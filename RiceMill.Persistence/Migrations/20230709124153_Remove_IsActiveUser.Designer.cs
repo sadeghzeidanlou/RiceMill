47a00cc6cead17233c548d699131ea9f7f701522
sadeghzeidanlou <sadeghzeidan@gmail.com>
2023-07-09 16:13:46 +0330

    Add migration for remove IsActive User

--- RiceMill.Persistence/Migrations/20230709124153_Remove_IsActiveUser.Designer.cs
+++ RiceMill.Persistence/Migrations/20230709124153_Remove_IsActiveUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RiceMill.Persistence;
 
@@ -11,9 +12,11 @@ using RiceMill.Persistence;
 namespace RiceMill.Persistence.Migrations
 {
     [DbContext(typeof(RiceMillDbContext))]
-    partial class RiceMillDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230709124153_Remove_IsActiveUser")]
+    partial class Remove_IsActiveUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RiceMill.Persistence/Migrations/20230709124153_Remove_IsActiveUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RiceMill.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_IsActiveUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
