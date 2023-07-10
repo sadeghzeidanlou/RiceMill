22bce751a78d77437fd63d6beb956fc1afd0bbec
sadeghzeidanlou <sadeghzeidan@gmail.com>
2023-07-10 08:45:39 +0330

    Implement caching

---
+++ RiceMill.Application/Common/Interfaces/ICacheService.cs
@@ -0,0 +1,11 @@
+﻿namespace RiceMill.Application.Common.Interfaces
+{
+    public interface ICacheService
+    {
+        T Get<T>(string key);
+
+        void Set<T>(string key, T value);
+
+        void Remove(string key);
+    }
+}
---
+++ RiceMill.Persistence/Caching/CacheService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using RiceMill.Application.Common.Interfaces;
+
+namespace RiceMill.Persistence.Caching
+{
+    public class CacheService : ICacheService
+    {
+        private readonly IMemoryCache _cache;
+
+        public CacheService(IMemoryCache cache) => _cache = cache;
+
+        public T Get<T>(string key) => _cache.Get<T>(key);
+
+        public void Set<T>(string key, T value) => _cache.Set(key, value, TimeSpan.MinValue);
+
+        public void Remove(string key) => _cache.Remove(key);
+    }
+}
