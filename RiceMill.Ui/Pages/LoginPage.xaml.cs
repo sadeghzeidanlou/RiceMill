62727400337ddc464f864d8b3d47d56fa4fc70d7
Sadegh Zeidanlou <46355431+sadeghzeidanlou@users.noreply.github.com>
2023-08-21 20:09:15 +0330

    Do Some work on UI

---
+++ RiceMill.Ui/Pages/LoginPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace RiceMill.Ui
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnBtnLoginClicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
