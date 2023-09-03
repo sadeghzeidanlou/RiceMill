3879ba88fd32c8a93e42953778f438e68aa6b9cc
Sadegh Zeidanlou <46355431+sadeghzeidanlou@users.noreply.github.com>
2023-09-03 23:55:57 +0330

    123

--- RiceMill.Ui/Pages/Concern/ConcernListPage.xaml.cs
+++ RiceMill.Ui/Pages/Concern/ConcernListPage.xaml.cs
@@ -1,28 +1,38 @@
+using RiceMill.Application.UseCases.ConcernServices.Dto;
+using RiceMill.Ui.Services.UseCases.ConcernServices;
+
 namespace RiceMill.Ui.Pages.Concern;
 
 public partial class ConcernListPage : ContentPage
 {
+    private readonly IConcernServices _concernServices;
+    private List<DtoConcern> Concerns;
+
     public ConcernListPage()
     {
         InitializeComponent();
+        _concernServices = new ConcernServices();
     }
 
-    private void RemoveButton_Clicked(object sender, EventArgs e)
+    protected override async void OnAppearing()
+    {
+        var concerns = await _concernServices.Get(new DtoConcernFilter());
+        Concerns = concerns.Data.Items;
+        ConcernCollectionView.ItemsSource = Concerns;
+        base.OnAppearing();
+    }
+
+    private void OnBtnSaveClicked(object sender, EventArgs e)
     {
 
     }
 
-    private void SaveButton_Clicked(object sender, EventArgs e)
-    {
- 
-    }
-
-    private void AddButton_Clicked(object sender, EventArgs e)
+    private void OnBtnRemoveClicked(object sender, EventArgs e)
     {
 
     }
 
-    private void NewButton_Clicked(object sender, EventArgs e)
+    private void OnNewBtnClicked(object sender, EventArgs e)
     {
 
     }
